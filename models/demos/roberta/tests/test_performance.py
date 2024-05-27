68448abb300d3cba1ba671ea1c38c43eeb744f80
kkeerthana0573 <kola.keerthana@multicorewareinc.com>
2024-05-27 18:58:38 +0000

    #6344: Update RoBERTa QA demo

--- models/demos/roberta/tests/test_performance.py
+++ models/demos/roberta/tests/test_performance.py
@@ -2,25 +2,18 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-import time
-
-import pytest
-
-from loguru import logger
-import torch
-import transformers
-
 
 import ttnn
+import time
+import torch
+import pytest
+import transformers
 
-from models.demos.bert.tt import ttnn_bert
+from loguru import logger
 from models.demos.bert.tt import ttnn_optimized_bert
-
 from ttnn.model_preprocessing import preprocess_model_parameters
 
 from models.utility_functions import (
-    is_wormhole_b0,
-    is_blackhole,
     enable_persistent_kernel_cache,
     disable_persistent_kernel_cache,
 )
@@ -29,18 +22,22 @@ from models.perf.perf_utils import prep_perf_report
 
 def get_expected_times(bert):
     return {
-        ttnn_bert: (13, 32),
         ttnn_optimized_bert: (12, 0.092),
     }[bert]
 
 
-@pytest.mark.skipif(is_wormhole_b0() or is_blackhole(), reason="Unsupported on WH and BH")
+def create_position_ids_from_input_ids(input_ids, padding_idx, past_key_values_length=0):
+    mask = input_ids.ne(padding_idx).int()
+    incremental_indices = (torch.cumsum(mask, dim=1).type_as(mask) + past_key_values_length) * mask
+    return incremental_indices.long() + padding_idx
+
+
 @pytest.mark.models_performance_bare_metal
 @pytest.mark.models_performance_virtual_machine
 @pytest.mark.parametrize("model_name", ["deepset/roberta-large-squad2"])
 @pytest.mark.parametrize("batch_size", [8])
 @pytest.mark.parametrize("sequence_size", [384])
-@pytest.mark.parametrize("bert", [ttnn_bert, ttnn_optimized_bert])
+@pytest.mark.parametrize("bert", [ttnn_optimized_bert])
 def test_performance(device, use_program_cache, model_name, batch_size, sequence_size, bert):
     disable_persistent_kernel_cache()
 
@@ -48,15 +45,10 @@ def test_performance(device, use_program_cache, model_name, batch_size, sequence
 
     input_ids = torch.randint(0, config.vocab_size, (batch_size, sequence_size)).to(torch.int32)
     torch_token_type_ids = torch.zeros((batch_size, sequence_size), dtype=torch.int32)
-    torch_position_ids = torch.zeros((batch_size, sequence_size), dtype=torch.int32)
     torch_attention_mask = torch.zeros(1, sequence_size) if bert == ttnn_optimized_bert else None
+    torch_position_ids = create_position_ids_from_input_ids(input_ids=input_ids, padding_idx=config.pad_token_id)
 
-    if bert == ttnn_bert:
-        tt_model_name = f"ttnn_{model_name}"
-    elif bert == ttnn_optimized_bert:
-        tt_model_name = f"ttnn_{model_name}_optimized"
-    else:
-        raise ValueError(f"Unknown functional_roberta: {bert}")
+    tt_model_name = f"ttnn_{model_name}_optimized"
 
     parameters = preprocess_model_parameters(
         model_name=tt_model_name,
